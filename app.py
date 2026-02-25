04f9106511a8ac599060262adbc2606412a7eb03
Tanmaya Pradhan <53402268+tanmayamj41@users.noreply.github.com>
2026-02-25 15:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Platform Engineering Demo Running!"
+    return "Platform Engineering Demo Running with different stages!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
